ca98be61fa4d0f0e21210ad2d4d95861d40b4e48
SakibSibly <sibly@duck.com>
2026-03-03 14:50:49 +0600

    Update "Sample message added to check conflict"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+    ### This is comment from br-2
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
