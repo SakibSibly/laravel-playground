551c858aff8f290d56f8a8013a186ca2fa1fd87e
SakibSibly <sibly@duck.com>
2026-03-04 10:44:26 +0600

    Update "`health` and `fallback` route implemented with function"

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 
 Route::apiResource('/items', ItemController::class);
+
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
+
+Route::fallback(function () {
+    return response()->json(['message' => 'Not Found'], 404);
+});
